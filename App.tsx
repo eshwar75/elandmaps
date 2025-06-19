d14ad6b441b9d3bc2b58e9e6ef479ff3c4f7556c
eshwar75 <raoe4165@gmail.com>
2025-06-20 00:30:43 +0530

    feat: new maps screen set up

--- App.tsx
+++ App.tsx
@@ -5,24 +5,68 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+import { requestStartUpPermission } from './src/Utils';
+import { Platform } from 'react-native';
+import LiveTrackingMapScreen from './src/screens/HomeScreen';
+export type RootStackParamList = {
+	Home: undefined;
+	Details: undefined;
+	LiveTrackingMap: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+	React.useEffect(() => {
+		const requestPermission = async () => {
+			try {
+				if (Platform.OS === 'android') {
+					const permission = await requestStartUpPermission();
+					if (
+						permission &&
+						typeof permission === 'object' &&
+						permission['android.permission.ACCESS_FINE_LOCATION'] === 'granted'
+					) {
+						requestStartUpPermission();
+						// } else {
+						// 	RequestBackgroundPermission();
+					}
+					// if (
+					// 	permission &&
+					// 	typeof permission === 'object' &&
+					// 	permission['android.permission.ACCESS_FINE_LOCATION'] !== 'granted'
+					// ) {
+					// 	startLocationTracking();
+					// }
+				}
+			} catch (error) {
+				console.error('Error requesting permission:', error);
+			}
+		};
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+		requestPermission();
+	}, []);
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{ animation: 'none', headerShown: false }}
+				initialRouteName="Home"
+			>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+				<Stack.Screen
+					name="LiveTrackingMap"
+					component={LiveTrackingMapScreen}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
